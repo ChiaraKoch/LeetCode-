841de28b05f723efcfbba2009ddc3d922d22cee6
Chiara Koch <149488467+ChiaraKoch@users.noreply.github.com>
2024-10-29 22:43:48 +0200

    Create product_arr_except_self.js

---
+++ product_arr_except_self.js
@@ -0,0 +1,33 @@
+// 238. Product of Array Except Self
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// Constraints:
+
+// 2 <= nums.length <= 105
+// -30 <= nums[i] <= 30
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let result = new Array(n).fill(1);
+
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+};
